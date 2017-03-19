7fc039e425a04d8c13c28943839985fa0d5f7dba
ZsZs <zsolt.zsuffa@gmail.com>
2017-03-19 20:08:04 +0100

    Implement SymbolType for FitPrint.

---
+++ fit-print-plugin/src/main/java/com/processpuzzle/fitnesse/print/FitPrintPlugin.java
@@ -0,0 +1,11 @@
+package com.processpuzzle.fitnesse.print;
+
+import fitnesse.plugins.PluginException;
+import fitnesse.plugins.PluginFeatureFactoryBase;
+import fitnesse.wikitext.parser.SymbolProvider;
+
+public class FitPrintPlugin extends PluginFeatureFactoryBase {
+   public void registerSymbolTypes( SymbolProvider symbolProvider ) throws PluginException {
+      symbolProvider.add( new FitToPdfSymbol() );
+   }
+}
